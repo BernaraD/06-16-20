c79c88828fd8d9770783b1a1f343263fe30ec27d
Bernara Dzhaparova <57506492+Betty8Bee@users.noreply.github.com>
2020-07-20 14:08:47 -0400

    Updates

---
+++ precache-manifest.71ea99756f8fe6977273967c07762006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387cdf5b47f50ae6f304b5429ea7cb6d",
+    "url": "/06-16-20/index.html"
+  },
+  {
+    "revision": "7a78cfd24b5dd7db41f0",
+    "url": "/06-16-20/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "186083ded3c0cdb76847",
+    "url": "/06-16-20/static/css/main.5f213ec9.chunk.css"
+  },
+  {
+    "revision": "7a78cfd24b5dd7db41f0",
+    "url": "/06-16-20/static/js/2.1fb5e018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/06-16-20/static/js/2.1fb5e018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186083ded3c0cdb76847",
+    "url": "/06-16-20/static/js/main.f4464adf.chunk.js"
+  },
+  {
+    "revision": "c7468f74b56536053e09",
+    "url": "/06-16-20/static/js/runtime-main.f5480bfa.js"
+  }
+]);
